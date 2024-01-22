e76002694ab11bbc434ca7ca7d22c63c7204d5d7
Ronit Singh <ronitsingh7003@gmail.com>
2024-01-22 14:13:11 +0530

    Use Router Hook

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import LearnLink from "@/components/LearnLink";
+import LearnUseRouter from "@/components/LearnUseRouter";
 
 export default function Home() {
   return (
     <>
       <div class="container mx-auto">
           <LearnLink />
+          <LearnUseRouter />
       </div>
     </>
   );
---
+++ src/components/LearnUseRouter.jsx
@@ -0,0 +1,16 @@
+"use client"
+import React from 'react'
+import { useRouter } from 'next/navigation'
+
+const LearnUseRouter = () => {
+    const router = useRouter()
+    // console.log("Router : ", router)
+    return (
+        <>
+            <h1>LearnUseRouter</h1>
+            <button onClick={() => router.push("/admin/dashboard")} type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Go to Admin Dashboard</button>
+        </>
+    )
+}
+
+export default LearnUseRouter
